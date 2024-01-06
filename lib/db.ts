742aa4f6755b8e9ca54f6c65f10f4b678d79c29e
RelexSun <88622289+RelexSun@users.noreply.github.com>
2024-01-06 18:48:02 +0700

    start prisma

--- lib/db.ts
+++ lib/db.ts
@@ -1,1 +1,15 @@
 import { PrismaClient } from "@prisma/client";
+
+// DO ✅
+declare global {
+  var prisma: PrismaClient | undefined;
+}
+
+export const db = globalThis.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") globalThis.prisma = db;
+
+// TODO: whenever we save a file nextjs will do a hot reload and it will create a new PrismaClient every time.
+
+// Don't❌
+// export const db = new PrismaClient()
