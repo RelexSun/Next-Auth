73441ddf29ebb49d07a47e858de1bfd4dd69bcc2
RelexSun <88622289+RelexSun@users.noreply.github.com>
2024-01-09 20:38:19 +0700

    callback

--- auth.ts
+++ auth.ts
@@ -1,5 +1,9 @@
 import NextAuth from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
+import { UserRole } from "@prisma/client";
+
+import { getUserById } from "@/data/user";
+
 import { db } from "@/lib/db";
 import authConfig from "@/auth.config";
 
@@ -9,6 +13,27 @@ export const {
   signIn,
   signOut,
 } = NextAuth({
+  callbacks: {
+    async session({ token, session }) {
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+
+      if (token.role && session.user) {
+        session.user.role = token.role as UserRole;
+      }
+      return session;
+    },
+    async jwt({ token }) {
+      if (!token.sub) return token;
+
+      const existingUser = await getUserById(token.sub);
+
+      if (!existingUser) return token;
+      token.role = existingUser.role;
+      return token;
+    },
+  },
   adapter: PrismaAdapter(db), // doesnt support edge
   session: { strategy: "jwt" },
   ...authConfig,
