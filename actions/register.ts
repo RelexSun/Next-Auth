55fe5dcad41567c7639a1e627092bdaf15446de1
RelexSun <88622289+RelexSun@users.noreply.github.com>
2024-01-12 00:56:29 +0700

    starting email verification

--- actions/register.ts
+++ actions/register.ts
@@ -7,6 +7,7 @@ import * as z from "zod";
 import { db } from "@/lib/db";
 import { getUserByEmail } from "@/data/user";
 import { generateVerificationToken } from "@/lib/token";
+import { sendVerificationEmail } from "@/lib/mail";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validatedFields = RegisterSchema.safeParse(values);
@@ -32,7 +33,7 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
 
   const verificationToken = await generateVerificationToken(email);
 
-  // TODO: Send verification token email
+  await sendVerificationEmail(verificationToken.email, verificationToken.token);
 
   return { success: "Confirmation email sent!" };
 };
